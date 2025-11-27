59189ef8f1676c39bb939c595ec5ee3bf0f1d79c
TimN1987 <timothy.naish@gmail.com>
2025-11-27 18:59:14 +0000

    Set up SoundService to pass sound effects to the MainWindow when requested.

--- Battleships/MVVM/Services/SoundService.cs
+++ Battleships/MVVM/Services/SoundService.cs
@@ -8,107 +8,111 @@ using System.Windows.Navigation;
 using Battleships.MVVM.Enums;
 using Battleships.MVVM.Utilities;
 using Battleships.MVVM.ViewModel;
+using Battleships.MVVM.View;
 
-namespace Battleships.MVVM.Services
+namespace Battleships.MVVM.Services;
+
+/// <summary>
+/// Interface for the SoundService. It defines the properties that the SoundService uses to load and play 
+/// the correct foreground and background sounds.
+/// </summary>
+public interface ISoundService
 {
-    /// <summary>
-    /// Interface for the SoundService. It defines the properties that the SoundService uses to load and play 
-    /// the correct foreground and background sounds.
-    /// </summary>
-    public interface ISoundService
+    public Uri CurrentBackgroundMusic { get; }
+    public int BackgroundMusicIndex { get; set; }
+    public Uri? CurrentForegroundSound { get; set; }
+}
+
+/// <summary>
+/// The SoundService is responsible for loading and playing the background music and foreground sounds. It 
+/// uses EventAggregator to subscribe to events that tell it which foreground sound to play or when the 
+/// background music is ended. It is then able to send the correct Uri to the <see cref="MainViewModel"/> 
+/// to load to the appropriate MediaElement.
+/// </summary>
+public class SoundService : ISoundService
+{
+    #region Fields
+    private readonly IEventAggregator _eventAggregator;
+    private readonly Uri _defaultBackgroundMusic;
+    private readonly List<Uri> _backgroundMusicList;
+    private readonly int _backgroundMusicListSize;
+    private int _backgroundMusicIndex;
+    private Uri? _currentForegroundSound;
+    private readonly Dictionary<GameEvent, Uri[]> _foregroundSounds;
+    #endregion //Fields
+
+    #region Properties
+    public Uri CurrentBackgroundMusic
     {
-        public Uri CurrentBackgroundMusic { get; }
-        public int BackgroundMusicIndex { get; set; }
-        public Uri? CurrentForegroundSound { get; set; }
+        get
+        {
+            if (_backgroundMusicListSize > 0)
+                return _backgroundMusicList[BackgroundMusicIndex];
+            return _defaultBackgroundMusic;
+        }
+    }
+    public int BackgroundMusicIndex
+    {
+        get => _backgroundMusicIndex;
+        set
+        {
+            if (_backgroundMusicIndex != value && value >= 0 && value < _backgroundMusicListSize)
+                _backgroundMusicIndex = value;
+        }
+    }
+    public Uri? CurrentForegroundSound
+    {
+        get => _currentForegroundSound;
+        set => _currentForegroundSound = value;
+    }
+    #endregion //Properties
+
+    public SoundService(IEventAggregator eventAggregator)
+    {
+        _eventAggregator = eventAggregator;
+        _eventAggregator.GetEvent<GameEventEvent>().Subscribe(param =>
+        {
+            if (param is GameEvent gameEvent)
+                OnGameEventReceived(gameEvent);
+        });
+        _eventAggregator.GetEvent<BackgroundMusicEndedEvent>().Subscribe(() => PublishNextBackgroundMusic());
+
+        _defaultBackgroundMusic = new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingone.wav", UriKind.Absolute); //update to correct music
+        _backgroundMusicList = [
+            new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingone.wav", UriKind.Absolute),
+            new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingtwo.wav", UriKind.Absolute),
+            new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingthree.wav", UriKind.Absolute)
+            ];
+        _backgroundMusicListSize = _backgroundMusicList.Count;
+        _backgroundMusicIndex = RandomProvider.Instance.Next(_backgroundMusicListSize);
+        _foregroundSounds = []; //add the sounds here when stored in resources
     }
 
     /// <summary>
-    /// The SoundService is responsible for loading and playing the background music and foreground sounds. It 
-    /// uses EventAggregator to subscribe to events that tell it which foreground sound to play or when the 
-    /// background music is ended. It is then able to send the correct Uri to the <see cref="MainViewModel"/> 
-    /// to load to the appropriate MediaElement.
+    /// Increments the background music index to the next song in the list. If the index is at the end of 
+    /// the list, it resets to 0. This is used when the background music ends to load the next song.
     /// </summary>
-    public class SoundService : ISoundService
+    internal void PublishNextBackgroundMusic()
     {
-        #region Fields
-        private readonly IEventAggregator _eventAggregator;
-        private readonly Uri _defaultBackgroundMusic;
-        private readonly List<Uri> _backgroundMusicList;
-        private readonly int _backgroundMusicListSize;
-        private int _backgroundMusicIndex;
-        private Uri? _currentForegroundSound;
-        private readonly Dictionary<ForegroundSoundType, Uri> _foregroundSounds;
-        #endregion //Fields
+        if (BackgroundMusicIndex < _backgroundMusicListSize - 1)
+            BackgroundMusicIndex++;
+        else
+            BackgroundMusicIndex = 0;
+        _eventAggregator.GetEvent<NextSongEvent>().Publish(CurrentBackgroundMusic);
+    }
 
-        #region Properties
-        public Uri CurrentBackgroundMusic
-        {
-            get
-            {
-                if (_backgroundMusicListSize > 0)
-                    return _backgroundMusicList[BackgroundMusicIndex];
-                return _defaultBackgroundMusic;
-            }
-        }
-        public int BackgroundMusicIndex
-        {
-            get => _backgroundMusicIndex;
-            set
-            {
-                if (_backgroundMusicIndex != value && value >= 0 && value < _backgroundMusicListSize)
-                    _backgroundMusicIndex = value;
-            }
-        }
-        public Uri? CurrentForegroundSound
-        {
-            get => _currentForegroundSound;
-            set => _currentForegroundSound = value;
-        }
-        #endregion //Properties
+    /// <summary>
+    /// Publishes a <see cref="LoadSoundEvent"/> with the Uri for a randomly selected sound effect for the 
+    /// given <see cref="GameEvent"/> to be played by a MediaElement in the <see cref="PlayGameView"/>.
+    /// </summary>
+    internal void OnGameEventReceived(GameEvent gameEvent)
+    {
+        Uri[] sounds = _foregroundSounds[gameEvent];
+        int availableSounds = sounds.Length;
+        int index = RandomProvider.Instance.Next(availableSounds);
 
-        public SoundService(IEventAggregator eventAggregator)
-        {
-            _eventAggregator = eventAggregator;
-            _eventAggregator.GetEvent<ForegroundSoundEvent>().Subscribe(param =>
-            {
-                if (param is ForegroundSoundType foregroundSound)
-                    OnForegroundSoundReceived(foregroundSound);
-            });
-            _eventAggregator.GetEvent<BackgroundMusicEndedEvent>().Subscribe(() => PublishNextBackgroundMusic());
-
-            _defaultBackgroundMusic = new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingone.wav", UriKind.Absolute); //update to correct music
-            _backgroundMusicList = [
-                new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingone.wav", UriKind.Absolute),
-                new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingtwo.wav", UriKind.Absolute),
-                new(@"pack://siteoforigin:,,,/MVVM/Resources/BackgroundMusic/backingthree.wav", UriKind.Absolute)
-                ];
-            _backgroundMusicListSize = _backgroundMusicList.Count;
-            _backgroundMusicIndex = RandomProvider.Instance.Next(_backgroundMusicListSize);
-            _foregroundSounds = []; //add the sounds here when stored in resources
-        }
-
-        /// <summary>
-        /// Increments the background music index to the next song in the list. If the index is at the end of 
-        /// the list, it resets to 0. This is used when the background music ends to load the next song.
-        /// </summary>
-        internal void PublishNextBackgroundMusic()
-        {
-            if (BackgroundMusicIndex < _backgroundMusicListSize - 1)
-                BackgroundMusicIndex++;
-            else
-                BackgroundMusicIndex = 0;
-            _eventAggregator.GetEvent<NextSongEvent>().Publish(CurrentBackgroundMusic);
-        }
-
-        /// <summary>
-        /// Loads the foreground sound based on the type of sound that is passed in. The sound is loaded from 
-        /// the <see cref="_foregroundSounds"/> dictionary and sent as a <see cref="LoadSoundEvent"/>./>
-        /// </summary>
-        /// <param name="soundToPlay"></param>
-        internal void OnForegroundSoundReceived(ForegroundSoundType soundToPlay)
-        {
-            var uriToLoad = _foregroundSounds[soundToPlay];
-            _eventAggregator.GetEvent<LoadSoundEvent>().Publish(uriToLoad);
-        }
+        _eventAggregator
+            .GetEvent<LoadSoundEvent>()
+            .Publish(sounds[index]);
     }
 }
--- Battleships/MVVM/View/MainWindow.xaml.cs
+++ Battleships/MVVM/View/MainWindow.xaml.cs
@@ -14,63 +14,62 @@ using System.Windows.Threading;
 using Battleships.MVVM.Services;
 using Battleships.MVVM.ViewModel;
 
-namespace Battleships
+namespace Battleships;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private readonly IEventAggregator _eventAggregator;
+
+    public MainWindow(MainViewModel viewModel, IEventAggregator eventAggregator)
     {
-        private readonly IEventAggregator _eventAggregator;
+        InitializeComponent();
+        DataContext = viewModel;
 
-        public MainWindow(MainViewModel viewModel, IEventAggregator eventAggregator)
+        _eventAggregator = eventAggregator;
+        _eventAggregator.GetEvent<FullScreenEvent>().Subscribe(isFullScreen => ChangeFullScreenSetting(isFullScreen));
+
+        WindowStyle = WindowStyle.None;
+        WindowState = WindowState.Maximized;
+
+        Loaded += MainWindow_Loaded;
+        KeyDown += KeyDown_Escape;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var storyboard = (Storyboard)FindResource("LoadingScreenFade");
+        storyboard.Begin();
+    }
+
+    private void ChangeFullScreenSetting(bool isFullScreen)
+    {
+        if (isFullScreen)
         {
-            InitializeComponent();
-            DataContext = viewModel;
-
-            _eventAggregator = eventAggregator;
-            _eventAggregator.GetEvent<FullScreenEvent>().Subscribe(isFullScreen => ChangeFullScreenSetting(isFullScreen));
-
+            WindowStyle = WindowStyle.SingleBorderWindow;
+            WindowState = WindowState.Normal;
+        }
+        else
+        {
             WindowStyle = WindowStyle.None;
             WindowState = WindowState.Maximized;
-
-            Loaded += MainWindow_Loaded;
-            KeyDown += KeyDown_Escape;
         }
+    }
 
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            var storyboard = (Storyboard)FindResource("LoadingScreenFade");
-            storyboard.Begin();
-        }
+    private void BackgroundMusic_MediaEnded(object sender, RoutedEventArgs e)
+    {
+        _eventAggregator.GetEvent<BackgroundMusicEndedEvent>().Publish();
+    }
 
-        private void ChangeFullScreenSetting(bool isFullScreen)
+    private void KeyDown_Escape(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
         {
-            if (isFullScreen)
-            {
-                WindowStyle = WindowStyle.SingleBorderWindow;
-                WindowState = WindowState.Normal;
-            }
-            else
-            {
-                WindowStyle = WindowStyle.None;
-                WindowState = WindowState.Maximized;
-            }
-        }
-
-        private void BackgroundMusic_MediaEnded(object sender, RoutedEventArgs e)
-        {
-            _eventAggregator.GetEvent<BackgroundMusicEndedEvent>().Publish();
-        }
-
-        private void KeyDown_Escape(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Escape)
-            {
-                ChangeFullScreenSetting(true);
-                
-                _eventAggregator.GetEvent<KeyDownFullScreenEvent>().Publish();
-            }
+            ChangeFullScreenSetting(true);
+            
+            _eventAggregator.GetEvent<KeyDownFullScreenEvent>().Publish();
         }
     }
 }
