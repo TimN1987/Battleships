b3d8d0bd5e83a436d8ac9c6eba3974c8d9195ed4
TimN1987 <timothy.naish@gmail.com>
2025-12-13 19:39:56 +0000

    Player and computer win messages added.

--- Battleships/MVVM/Utilities/GameOverProvider.cs
+++ Battleships/MVVM/Utilities/GameOverProvider.cs
@@ -2,6 +2,26 @@
 
 public static class GameOverProvider
 {
+    // Player wins messages
+    const string PlayerWinMessageOne = "Congratulations! You've sunk all enemy ships and won the battle!";
+    const string PlayerWinMessageTwo = "Victory is yours! The enemy fleet has been defeated!";
+    const string PlayerWinMessageThree = "Well done, Captain! You've outmaneuvered the enemy and secured a win!";
+    const string PlayerWinMessageFour = "You've emerged victorious! The seas are yours!";
+    const string PlayerWinMessageFive = "Fantastic job! You've successfully destroyed the enemy fleet!";
+
+    // Computer wins messages
+    const string ComputerWinMessageOne = "Defeat! The enemy fleet has sunk all your ships!";
+    const string ComputerWinMessageTwo = "The enemy has won! Your fleet has been destroyed!";
+    const string ComputerWinMessageThree = "Alas, Captain! The enemy has outmaneuvered you and claimed victory!";
+    const string ComputerWinMessageFour = "The seas belong to the enemy now! Your ships have been sunk!";
+    const string ComputerWinMessageFive = "The enemy fleet has triumphed! Better luck next time!";
+
+    // Player wins gifs
+
+    // Computer wins gifs
+
+
+    // Static fields
     private static bool _playerWins = false;
     private static int _messageIndex = 0;
     private static string[] _playerWinsMessages = [];
@@ -9,10 +29,6 @@ public static class GameOverProvider
     private static int _gifIndex = 0;
     private static Uri[] _playerWinsGifs = [];
     private static Uri[] _computerWinsGifs = [];
-
-    // Constant messages
-
-    // Game over gifs
 
     // Methods
 
@@ -23,6 +39,10 @@ public static class GameOverProvider
     public static string GetGameOverMessage(bool playerWins)
     {
         _playerWins = playerWins;
+        _messageIndex = RandomProvider.Instance.Next(
+            0,
+            _playerWins ? _playerWinsMessages.Length : _computerWinsMessages.Length
+        );
         return _playerWins ? _playerWinsMessages[_messageIndex] : _computerWinsMessages[_messageIndex];
     }
 
@@ -33,6 +53,10 @@ public static class GameOverProvider
     public static Uri GetGameOverGif(bool playerWins)
     {
         _playerWins = playerWins;
+        _gifIndex = RandomProvider.Instance.Next(
+            0,
+            _playerWins ? _playerWinsGifs.Length : _computerWinsGifs.Length
+        );
         return _playerWins ? _playerWinsGifs[_gifIndex] : _computerWinsGifs[_gifIndex];
 
     }
