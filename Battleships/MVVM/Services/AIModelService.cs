7f74266580297a7ac71e7d22bdd55c2c620c4b28
TimN1987 <timothy.naish@gmail.com>
2025-12-18 09:36:32 +0000

    Changed to scoped namespace.

--- Battleships/MVVM/Services/AIModelService.cs
+++ Battleships/MVVM/Services/AIModelService.cs
@@ -7,14 +7,14 @@ namespace Battleships.MVVM.Services;
 
 public interface IAIModelService
 {
-    int SelectNextSHot(GameStateDTO gameStateDTO, out ShotType shotType);
+    int SelectNextShot(GameStateDTO gameStateDTO, out ShotType shotType);
 }
 
 public class AIModelService : IAIModelService
 {
-    private readonly HttpClient _httpClient = new HttpClient();
+    private readonly HttpClient _httpClient = new();
 
-    public int SelectNextSHot(GameStateDTO gameStateDTO, out ShotType shotType)
+    public int SelectNextShot(GameStateDTO gameStateDTO, out ShotType shotType)
     {
         shotType = ShotType.Single;
         return 0;
